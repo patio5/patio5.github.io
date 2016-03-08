8476954032d7efb441a1ef16b78bd4fd8d5b8357
Skylanders SuperChargers Video Game - Official Site <pflp4student@KSPFL-Y421.local>
2016-03-08 16:24:52 +0800

    added some randomness to falling objects

--- game/js/index.js
+++ game/js/index.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
-  
-    $("#myButton1").click(function() {
-    $("#dropthingy").toggleClass("newclass");
-  
-});
+
+     setInterval(function(){ 
+    $("#dropthingy1").toggleClass("newclass");
+  }, 2000);
+
+        setInterval(function(){ 
+    $("#dropthingy8").toggleClass("newclass");
+  }, 4000);
+
+           setInterval(function(){ 
+    $("#dropthingyh").toggleClass("newclass");
+  }, 1000);
+
+              setInterval(function(){ 
+    $("#dropthingyg").toggleClass("newclass");
+  }, 8000);
+
 });
